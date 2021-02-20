ccb68363a1d886cac7bf681867807da84b83f28c
paul-frdev <pavelzadyrko@gmail.com>
2021-02-20 19:44:41 +0200

    added news-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,22 @@
 $(function () {
-  
-$('.select__list').styler()
 
-// $('..jq-selectbox select').()
+  $('.news-slider__items').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+  });
 
-var mixer = mixitup('.portfolio__content');
+  $('.select__list').styler()
+
+  // $('..jq-selectbox select').()
+
+  var mixer = mixitup('.portfolio__content');
+
+
 });
 
-foo.onclick = function(){
+foo.onclick = function () {
   this.classList.toggle('rotate')
 }
-
-
--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ function styles() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'node_modules/mixitup/dist/mixitup.js',
     'app/js/main.js'
