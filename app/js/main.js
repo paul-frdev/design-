734340526d6a410b28fb5a5b9f4b18d248d7a9ff
paul-frdev <pavelzadyrko@gmail.com>
2021-02-21 22:57:45 +0200

    added review-slider, added footer

--- app/js/main.js
+++ app/js/main.js
@@ -8,15 +8,18 @@ $(function () {
     nextArrow: '<button type="button" class="slick-next"></button>',
   });
 
-  $('.select__list').styler()
+  $('.select__list').styler();
 
-  // $('..jq-selectbox select').()
+  $('.review-slider__inner').slick({
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+  });
 
   var mixer = mixitup('.portfolio__content');
 
-
 });
-
-foo.onclick = function () {
-  this.classList.toggle('rotate')
-}
