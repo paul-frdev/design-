aa960d8ce456816104d5c6bfd396875ebe115e33
paul-frdev <pavelzadyrko@gmail.com>
2021-02-24 23:17:19 +0200

    fixed bags

--- app/js/main.js
+++ app/js/main.js
@@ -10,15 +10,15 @@ $(function () {
 
   $('.select__list').styler();
 
-  $('.review-slider__inner').slick({
+  $('.review-slider__items').slick({
     autoplay: false,
     autoplaySpeed: 2000,
     dots: true,
     slidesToShow: 3,
     slidesToScroll: 2,
-    prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>',
-  });
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/review/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/review/arrow-right.svg" alt="arrow-right"></button>',
+   });
 
   var mixer = mixitup('.portfolio__content');
 
