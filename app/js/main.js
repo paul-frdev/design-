500758422d8ef4a66eb6deb027c0c1ad237d6643
paul-frdev <pavelzadyrko@gmail.com>
2021-02-25 22:50:41 +0200

    fixed bags slider

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,13 @@ $(function () {
     dots: false,
     prevArrow: '<button type="button" class="slick-prev"></button>',
     nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [{
+      breakpoint: 1260,
+      settings: {
+        slidesToShow: 2,
+    slidesToScroll: 1,
+      }
+    }, ]
   });
 
   $('.select__list').styler();
@@ -18,8 +25,36 @@ $(function () {
     slidesToScroll: 2,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/review/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/review/arrow-right.svg" alt="arrow-right"></button>',
-   });
+    responsive: [{
+      breakpoint: 1350,
+      settings: {
+        centerMode: true,
+        variableWidth: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+      }
+    }, ]
+  });
 
   var mixer = mixitup('.portfolio__content');
 
 });
+
+
+var smy = document.getElementById("s1");
+var assilka = document.getElementById("a1");
+
+smy.onchange = function OnChange() {
+  switch (smy.selectedIndex) {
+    case 0:
+      assilka.href = "Selected-0";
+      break;
+    case 1:
+      assilka.href = "Selected-1";
+      break;
+    case 2:
+      assilka.href = "Selected-2";
+      break;
+  }
+}
